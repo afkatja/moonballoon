95b496f0386d061731fa422f4fd83ca2605bcf7d
Katja Hollar <afkatja@gmail.com>
2014-12-01 13:50:02 +0100

    commented everything out for the demo; allow maps to zoom

--- moonballoon.js
+++ moonballoon.js
@@ -13,14 +13,60 @@ if (Meteor.isClient) {
         positionBallon(id);
       }
     });
-    console.log(query, Coordinates.findOne());
+
+    Meteor.subscribe('logs');
+    var handle = Log.find().observeChanges({
+      added: function(id, fields) {
+        placemarker(id);
+      },
+      changed: function (id, fields) {
+        placemarker(id);
+      }
+    });
+    //console.log(query, Coordinates.findOne());
+    //positionBallon();
+    //placemarker();
   });
 
   function positionBallon(id){
-    console.log('positioning balloon', id);
+    console.log('positioning balloon', Log.findOne());
     var lastAlt = Coordinates.findOne(id).timestamp;
     $('input[type=range]').val(lastAlt/40000*100);
     $('.chosen-height span').text(lastAlt);
+  }
+
+  function center(lat, lng) {
+    if (window.map) {
+      console.log('centering', lat, lng);
+      window.map.setCenter({lat: lat, lng: lng});
+    }
+  }
+
+  function placemarker(id){
+    if (!window.markers) {
+      window.markers = [];
+    }
+    var lastLog = Log.findOne(id);
+    console.log('will place marker', id, lastLog);
+    if (window.markers[id]) {
+      var marker = window.markers[id];
+      marker.setPosition({ lat: lastLog.lat, lng: lastLog.lng });
+    } else {
+      try {
+        var marker = new google.maps.Marker({
+            position: { lat: lastLog.lat, lng: lastLog.lng },
+            map: window.map,
+            title: id,
+            zIndex: 1
+        });
+        window.markers[id] = marker;
+
+        console.log('created marker', lastLog.lat, lastLog.lng);
+      } catch(e) {
+        console.log('error', e);
+      }
+    }
+    center(lastLog.lat, lastLog.lng);
   }
 
   Template.location.helpers({
@@ -29,10 +75,32 @@ if (Meteor.isClient) {
     },
     stats: function(){
       var log = this;
-      var result = moment(log.timestamp).fromNow() + ', ' + log.alt;
-      return result;
+      var result = moment(log.timestamp).fromNow() + ', ' + log.alt + ', '+ log.pic;
+      return Log.find({}, {sort: {timestamp: -1}});
+    },
+    maps: function(){
+      return Coordinates.findOne();
     }
   });
+
+  Template.location.rendered = function(){
+    console.log('initializing maps', this);
+    GoogleMaps.init({},
+      function(){
+        var mapOptions = {
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          center: {lat: 52.071878, lng:4.345358},
+          //zoomControl: false,
+          streetViewControl: false,
+          panControl: false,
+          rotateControl: false,
+          //scaleControl: false,
+          scrollwheel: false
+        };
+        window.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      });
+  };
 
   Template.location.events({
     'change input[type=range]': function (e) {
@@ -57,24 +125,30 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     var json = HTTP.get('http://moonballoon.azurewebsites.net/get/position');
     //var data = JSON.parse(Assets.getText("m00nballoon.json"));
-    Log.remove();
-    Coordinates.remove();
+    Log.remove({});
+    Coordinates.remove({});
+    console.log(Coordinates.find().count(), json.length);
     if(Coordinates.find().count() === 0) {
-      for (var i = 0; i < json.length; i++) {
-        Log.insert({
-          lat: json[i].Lat,
-          lng: json[i].Lon,
-          alt: json[i].Alt,
-          pic: 'http://az695307.vo.msecnd.net/mycontainer/'+json[i].PhotoName,
-          timestamp: json[i].When
-        });
-        Coordinates.insert({
-          lat: json[i].Lat,
-          lng: json[i].Lon,
-          alt: json[i].Alt,
-          pic: 'http://az695307.vo.msecnd.net/mycontainer/'+json[i].PhotoName,
-          timestamp: json[i].When
-        });
+      if(json.statusCode == 200) {
+        var response = JSON.parse(json.content);
+        for (var i = 0; i < response.length; i++) {
+        console.log('json content', response[i].timestamp);
+          Log.insert({
+            lat: response[i].Lat,
+            lng: response[i].Lon,
+            alt: response[i].Alt,
+            pic: 'http://az695307.vo.msecnd.net/mycontainer/'+response[i].PhotoName,
+            timestamp: response[i].When
+          });
+
+          Coordinates.insert({
+            lat: response[i].Lat,
+            lng: response[i].Lon,
+            alt: response[i].Alt,
+            pic: 'http://az695307.vo.msecnd.net/mycontainer/'+response[i].PhotoName,
+            timestamp: response[i].When
+          });
+        }
       }
     }
   });
